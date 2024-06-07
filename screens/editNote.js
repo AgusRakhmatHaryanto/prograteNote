76185f847cf31a865eebb119345eff14b9370ec2
Agus Rakhmat Haryanto <xtkr2.03.agusrakhmatharyanto@gmail.com>
2024-06-07 16:04:12 +0700

    [agus]feat: add feature edit & delete

--- screens/editNote.js
+++ screens/editNote.js
@@ -1,6 +1,80 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import CustomButton from "../components/customButtom";
+import CustomTextInput from "../components/customTextInput";
 
-export default function EditNote() {
-  return <Text>Edit Note</Text>;
+export default function EditNote({ setCurrentPage, editNote, noteToEdit }) {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  useEffect(() => {
+    if (noteToEdit) {
+      setTitle(noteToEdit.title);
+      setDesc(noteToEdit.desc);
+    }
+  }, [noteToEdit]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>Edit Note</Text>
+      <CustomTextInput
+        text={title}
+        onChange={setTitle}
+        label="Judul"
+        placeholder="Judul"
+        numberOfLines={1}
+        multiline={false}
+      />
+      <CustomTextInput
+        text={desc}
+        onChange={setDesc}
+        label="Deskripsi"
+        placeholder="Deskripsi"
+        multiline
+        numberOfLines={4}
+      />
+      <View style={styles.spacerTop}>
+        <CustomButton
+          backgroundColor="#247881"
+          color="#fff"
+          text="Simpan"
+          width="100%"
+          onPress={() => {
+            editNote(noteToEdit.id, title, desc);
+            setCurrentPage("Home");
+          }}
+        />
+      </View>
+      <View style={styles.spacerTop}>
+        <CustomButton
+          backgroundColor="#DDDDDD"
+          color="#203239"
+          text="Kembali ke Home"
+          width="100%"
+          onPress={() => {
+            setCurrentPage("Home");
+          }}
+        />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 20,
+  },
+  pageTitle: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: "700",
+    textAlign: "center",
+    color: "#203239",
+  },
+  spacerTop: {
+    marginTop: 30,
+  },
+});
